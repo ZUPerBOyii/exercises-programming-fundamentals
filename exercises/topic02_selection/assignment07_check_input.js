7e86ac0d3d36a8ba268a93a862ab7f197a596a70
Rati Goginashvili <rati.goginashvili@student.howest.be>
2022-09-23 14:46:53 +0200

    Finished the check input exercise, last one of lab02

--- exercises/topic02_selection/assignment07_check_input.js
+++ exercises/topic02_selection/assignment07_check_input.js
@@ -13,3 +13,11 @@ Now let the user know if the input was a number or not.
 
 */
 import io from "../../utils/io-for-pf.js";
+let a = io.read();
+a = a / 1;
+
+if (a !== a){
+  io.write("No");
+}else{
+  io.write("Yes");
+}
