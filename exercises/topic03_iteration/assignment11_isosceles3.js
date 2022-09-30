1635b19d748149c3164f399ec95bf1b4a375d10b
Rati Goginashvili <rati.goginashvili@student.howest.be>
2022-09-30 09:50:38 +0200

    isosceles3 completed after finally understanding how to add spaces

--- exercises/topic03_iteration/assignment11_isosceles3.js
+++ exercises/topic03_iteration/assignment11_isosceles3.js
@@ -14,4 +14,20 @@ This time the triangle is right aligned and has its top at the bottom.
         *
 */
 import io from "../../utils/io-for-pf.js";
+let input = parseInt(io.read());
+let str = "*";
+let amountOfSpaces = 0;
+let spaces = " ";
 
+
+while(input <= 0){
+  input = parseInt(io.read());
+}
+
+
+for(let i = input; i >= 1; i--){
+  amountOfSpaces += 1;
+  io.write(spaces.repeat(amountOfSpaces) + str.repeat(i));
+
+
+}
