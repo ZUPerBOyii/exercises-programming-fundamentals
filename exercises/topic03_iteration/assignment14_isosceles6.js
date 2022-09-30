f29e84ca1ae5cc95bb1df338ba52cfbf8ce01228
Rati Goginashvili <rati.goginashvili@student.howest.be>
2022-09-30 10:04:19 +0200

    isosceles6 completed, same as 5 but i -= 2

--- exercises/topic03_iteration/assignment14_isosceles6.js
+++ exercises/topic03_iteration/assignment14_isosceles6.js
@@ -19,3 +19,18 @@ This time the triangle has its top at the bottom.
 */
 import io from "../../utils/io-for-pf.js";
 
+let input = parseInt(io.read());
+let str = "*";
+let amountOfSpaces = 0;
+let spaces = " ";
+
+
+while(input <= 0){
+  input = parseInt(io.read());
+}
+
+
+for(let i = input; i >= 1; i -= 2) {
+  amountOfSpaces += 1;
+  io.write(spaces.repeat(amountOfSpaces) + str.repeat(i));
+}
