07554bcf88a9e23f8e96384b04409e233f85ed14
Rati Goginashvili <rati.goginashvili@student.howest.be>
2022-09-30 09:52:57 +0200

    isosceles4 completed with ease this time

--- exercises/topic03_iteration/assignment12_isosceles4.js
+++ exercises/topic03_iteration/assignment12_isosceles4.js
@@ -15,3 +15,19 @@ This time triangle is right aligned and has its top on top.
 */
 import io from "../../utils/io-for-pf.js";
 
+let input = parseInt(io.read());
+let str = "*";
+let amountOfSpaces = input;
+let spaces = " ";
+
+
+while(input <= 0){
+  input = parseInt(io.read());
+}
+
+
+for(let i = 1; i <= input; i++) {
+  amountOfSpaces -= 1;
+  io.write(spaces.repeat(amountOfSpaces) + str.repeat(i));
+
+}
