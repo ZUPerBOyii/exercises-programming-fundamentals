c8c7da717aa44a675e7df0bc28ed6959a1406ad0
Rati Goginashvili <rati.goginashvili@student.howest.be>
2022-10-04 14:20:04 +0200

    Finished first exercise of arrays

--- exercises/topic04_lists/assignment01_reverse_sequence.js
+++ exercises/topic04_lists/assignment01_reverse_sequence.js
@@ -4,4 +4,19 @@ Write a program that asks for 100 numerical inputs and
 outputs them in reverse order.
 */
 import io from "../../utils/io-for-pf.js";
+let numbers = new Array();
+let nrCounter = 0;
+let maxCounter = 4;
+
+
+do {
+  let input = parseInt(io.read());
+  numbers[nrCounter] = input;
+  nrCounter++;
+}while(nrCounter <= maxCounter);
+
+for (let i = numbers.length - 1; i >= 0; i--){
+  io.write(numbers[i]);
+}
+
 
