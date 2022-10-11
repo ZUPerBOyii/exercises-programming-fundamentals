b671f8b4bb1109bcfad8dd301a374ea01ddb23c8
Rati Goginashvili <rati.goginashvili@student.howest.be>
2022-10-11 16:44:05 +0200

    Close to completing count sort, but not yet there

--- exercises/topic04_lists/assignment05_count_sort.js
+++ exercises/topic04_lists/assignment05_count_sort.js
@@ -4,7 +4,7 @@ Write a program that asks for numerical input (integers) between 0 and 99.
 When the user inputs a number outside this interval
 the program stops asking input.
 
-Print all the numbers that where entered, from small to large.
+Print all the numbers that were entered, from small to large.
 Numbers that were entered multiple times, however, are only printed once.
 
 ## Example
@@ -22,4 +22,23 @@ Numbers that were entered multiple times, however, are only printed once.
     5
 */
 import io from "../../utils/io-for-pf.js";
+
+const frequencyOf = new Array(100).fill(0);
+let number = parseInt(io.read());
+
+while(number >= 0 && number < frequencyOf.length) {
+  frequencyOf[number]++; //frequencyOf[number] = frequencyOf[number] + 1;
+  number = parseInt(io.read());
+}
+
+for (let i = 0; i < frequencyOf.length; i++){
+  if (frequencyOf[i] === 1){
+    io.write(frequencyOf[i] + i - 1);
+  }
+  else if (frequencyOf[i] > 1){
+    io.write(frequencyOf[i] - i);
+  }
+}
+
+
 
