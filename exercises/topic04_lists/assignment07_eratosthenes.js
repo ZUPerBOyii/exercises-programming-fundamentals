6f987624a0b2f023b67448ca56e2045c30d2bf55
Rati Goginashvili <rati.goginashvili@student.howest.be>
2022-10-14 14:32:22 +0200

    Finished eratosthenes

--- exercises/topic04_lists/assignment07_eratosthenes.js
+++ exercises/topic04_lists/assignment07_eratosthenes.js
@@ -17,3 +17,24 @@ Then, the program prints all prime-numbers below that upper bound.
 */
 import io from "../../utils/io-for-pf.js";
 
+let i;
+let n = parseInt(io.read());
+const primeNumbers = new Array(n).fill(1);
+
+while (n <= 1) {
+  n = parseInt(io.read());
+}
+for (i = 2; i <= Math.sqrt(n); i++) {
+  if (primeNumbers[i] === 1) {
+    for (let j = 2; i * j <= n; j++) {
+      primeNumbers[i * j] = 0;
+    }
+  }
+}
+for (i = 0; i <= primeNumbers.length; i++) {
+  if (primeNumbers[i] === 1) {
+    io.write(primeNumbers[i]);
+  }
+}
+
+
